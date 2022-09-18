d505ed339eac37c4b99babff331bb9cd28e5eb32
xprog <bezt.igor@gmail.com>
2022-09-18 22:36:47 +0300

    Task 17.1

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -24,10 +24,41 @@ switch,mac,ip,vlan,interface
 sw3,00:E9:BC:3F:A6:50,100.1.1.6,3,FastEthernet0/20
 sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
 
-Первый столбец в csv файле имя коммутатора надо получить из имени файла,
+Первый столбец в csv файле имя коммутатора надо получить из имени файла, c
 остальные - из содержимого в файлах.
 
 Проверить работу функции на содержимом файлов sw1_dhcp_snooping.txt,
 sw2_dhcp_snooping.txt, sw3_dhcp_snooping.txt.
 
 """
+
+import re
+import csv
+
+def write_dhcp_snooping_to_csv(files, output):
+    result_file = open(output, 'w')
+    writer = csv.writer(result_file)
+    headers = ['switch','mac','ip','vlan','interface']
+    writer.writerow(headers)
+
+    for file in files:
+        with open(file) as f:
+            switch = re.search(r'(\w+?)_.+', file).group(1)
+
+            lines = f.read()
+            # 00:09:BB:3D:D6:58   10.1.10.2        86250       dhcp-snooping   10    FastEthernet0/1
+            # data = re.finditer(r'^(?P<mac>\d.+?)\s+(?P<ip>\S+).*?(?P<intf>\w+/\d+)$', lines, re.MULTILINE)
+            data = re.finditer(r'^(?P<mac>\d.+?)\s+(?P<ip>\S+).*?(?P<vlan>\d+)\s+(?P<intf>\w+/\d+)$', lines, re.MULTILINE)
+            for d in data:
+                row = [switch, d['mac'], d['ip'], d['vlan'], d['intf']]
+                # row = [switch, *d]
+                # print(row)
+                writer.writerow(row)
+
+
+    result_file.close()
+    return None
+
+if __name__ == '__main__':
+    files = ['sw1_dhcp_snooping.txt', 'sw2_dhcp_snooping.txt', 'sw3_dhcp_snooping.txt']
+    write_dhcp_snooping_to_csv(files, 'output.csv')
