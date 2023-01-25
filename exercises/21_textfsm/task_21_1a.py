0c91535832f9b5c2987201ad6a3b2da55b5a9df5
xprog <bezt.igor@gmail.com>
2023-01-25 23:54:03 +0300

    task 21.1a

--- exercises/21_textfsm/task_21_1a.py
+++ exercises/21_textfsm/task_21_1a.py
@@ -16,3 +16,16 @@
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt
 и шаблоне templates/sh_ip_int_br.template.
 """
+import textfsm
+
+def parse_output_to_dict(template, command_output):
+    with open(template) as templ:
+        fsm = textfsm.TextFSM(templ)
+        result = fsm.ParseText(command_output)
+        return [dict(zip(fsm.header, data)) for data in result]
+
+if __name__ == "__main__":
+    with open('output/sh_ip_int_br.txt') as filedata:
+        data = filedata.read()
+        result = parse_output_to_dict("templates/sh_ip_int_br.template", data)
+        print(result)
